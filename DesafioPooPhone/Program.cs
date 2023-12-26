086ef55bf90a19ec78346f77334ac8be3024a29c
edsay12 <edvandearaujo2@hotmail.com>
2023-12-26 10:39:41 -0300

    Finalização do desafio2

---
+++ DesafioPooPhone/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+
+
+Nokia N = new Nokia("123123", "C3-rf", "12312312", 5);
+
+N.InstalarAplicativo("youtube");
+N.ReceberLigacao();
+
+
+Iphone p = new Iphone("123123", "C3-rf", "12312312", 3);
+p.Ligar();
+p.InstalarAplicativo("Facebook");
